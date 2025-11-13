f0f22c32a9e895318add306fabd2fdc0c4727228
CharaD7 <jijakahn6@gmail.com>
2025-11-13 00:41:17 +0000

    feat: setup Vue app with motion animations support

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import HumbleDataLanding from './components/HumbleDataLanding.vue'
+</script>
+
+<template>
+  <HumbleDataLanding />
+</template>
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import { MotionPlugin } from '@vueuse/motion'
+import 'animate.css'
+import './style.css'
+import App from './App.vue'
+
+createApp(App)
+  .use(MotionPlugin)
+  .mount('#app')
